b9eeedbda46865d6d46e7200604a63984dc405ce
Jay Dhanoa <jsdhanoa987@gmail.com>
2019-03-14 22:21:06 -0500

    updated documentation

--- pycode/demo1.sh
+++ pycode/demo1.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# run_qnet_local will iteratively call qNet in a subprocess to generate all of the decision trees and edge files
+# those files are stored in test_output
 python run_qnet_local.py ../ ../test_output --prefixes cchfl --cutoffs 75 --num_features 400
 # output generated in ../test_output
 
+# networkx_qnets will run on the .dot files generated in the first step and create a PNG of the qnet
+# the default resolution is sufficient for the cchfl example
 python3 networkx_qnets.py ../test_output --output_dir ../test_output --prefixes cchfl
 # output also stored there
--- pycode/demo2.sh
+++ pycode/demo2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# this perturbs the test sequence stored in cchfl_example_seq at location 35, amino acid A
+# we denote the same cutoffs that we generated in step 2 of demo1.sh
 python perturb_sequence.py ../test_output --prefixes cchfl ../test_output --target_location 135 --target_amino_acid A --cutoffs 75 --input_sequence ../cchfl_sample_seq.txt --end_subtype cchfl --end_cutoff 75 --output_file ../test_output/CCHFL_DEMO.pkl
 
+# generate perturbed sequences with the model saved by perturb_sequence
 python generate_perturbed_sequences.py ../cchfl_sample_seq.txt ../test_output/CCHFL_DEMO.pkl ../test_output/CCHFL_GENERATED.csv
